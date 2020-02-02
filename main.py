e678662f977f991ac659f23d9e5c47ba8c5c89b3
Kamran Mackey <kamranm1200@gmail.com>
2020-02-02 00:25:12 -0700

    Added sample configuration file.
    Also changed the way I log the "Logged in" message.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 
 import discord
 import toml
+from discord import Activity, ActivityType
 from discord.ext import commands
 from discord.ext.commands import AutoShardedBot, when_mentioned_or
 
@@ -14,8 +15,8 @@ bot = AutoShardedBot(command_prefix=when_mentioned_or(*prefixes))
 
 @bot.event
 async def on_ready():
-    print('Logged in as:')
-    print('Username: {0}'.format(bot.user.name))
-    print('ID: {0}'.format(bot.user.id))
+    activity = Activity(type=ActivityType.playing, name="test")
+    print('Logged in as {0}#{1}'.format(bot.user.name, bot.user.discriminator))
+    await bot.change_presence(activity=activity)
 
 bot.run(token, reconnect=True)
