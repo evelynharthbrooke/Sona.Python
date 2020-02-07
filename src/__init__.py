70739e0562a06be983406d0b691ccb2d14993019
Kamran Mackey <kamranm1200@gmail.com>
2020-02-07 01:38:01 -0700

    Added a VS Code workspace settings file.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 import subprocess
 
 VERSION = "0.1.0"
-GIT_SHA = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip().decode('utf8')
+GIT_SHA = subprocess.check_output(
+    ['git', 'rev-parse', '--short', 'HEAD']).strip().decode('utf8')
